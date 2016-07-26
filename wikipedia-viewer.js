bdbab49e2128495a1aafb8c0a6d6071c5a405109
Qian <pandachain731@gmail.com>
2016-07-25 17:00:45 -0700

    functions and style completed

---
+++ wikipedia-viewer.js
@@ -0,0 +1,68 @@
+/**
+ * Created by pandachain on 2016-07-22.
+ */
+
+var list = [];
+
+var cleanResult = function(){
+  if (list.length > 0){
+    document.querySelector(".searchResult").innerHTML = "";
+  }
+};
+
+var getKeyword = function(){
+  var searchBox = document.querySelector("#searchBox");
+  var keyword = searchBox.value;
+  callAPI(keyword);
+};
+
+var startSearch= function(){
+  document.querySelector("#searchBox").addEventListener("keypress", function(key){
+    if (key.keyCode === 13) {
+      document.location.href = "#top";
+
+      getKeyword();
+    }
+  });
+  document.querySelector(".fa-search").addEventListener("click", getKeyword)
+};
+
+
+var callAPI = function(keyword){
+
+  var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" + keyword;
+
+  var listRequest = new XMLHttpRequest();
+  listRequest.open('GET', url, true);
+
+  listRequest.onload = function () {
+    if (listRequest.status >= 200 && listRequest.status < 400) {
+      list = JSON.parse(listRequest.responseText)["query"]["search"];
+      cleanResult();
+      displayResult();
+    }
+  };
+  listRequest.onerror = function () {
+    document.querySelector(".searchResult").innerHTML = "<p>Sorry we were unable to reach the weather server..</p>"
+  };
+
+  listRequest.send();
+};
+
+var displayResult = function(){
+
+  function generateHTML (element, index, array) {
+    var title = "<h4>" + element["title"] + "</h4>" ;
+    var text = "<p>" + element["snippet"] + "</p>";
+    var displayBox = document.querySelector(".searchResult");
+      displayBox.innerHTML += '<a target="_blank" href="http://en.wikipedia.org/wiki/' 
+                                                        + element["title"] + '"><div class="resultDecor">' + title + text + "</div></a>";
+      
+    
+  }
+
+  list.forEach(generateHTML);
+
+};
+
+startSearch();
